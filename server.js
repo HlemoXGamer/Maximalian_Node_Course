0ef414f5e9fb71f3d77665ad77dc1032c0066ede
YoussefHlem <80886022+HlemoXGamer@users.noreply.github.com>
2023-12-01 16:29:02 +0200

    url module

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
+// http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     switch (url) {
+//         case "/":
+//             res.write('<form>"Post Message Form"</form>');
+//             res.end();
+//             break;
+//         case "/message":
+//             res.write("<h1>Hello World</h1>");
+//             if (method === "POST") {
+//                 const body = [];
+//                 req.on("data", (chunk) => {
+//                     console.log(chunk);
+//                     body.push(chunk);
+//                 });
+//                 req.on("end", () => {
+//                     const parsedBody = Buffer.concat(body).toString();
+//                     fs.writeFileSync("message.txt", parsedBody);
+//                 });
+//                 res.statusCode = 302;
+//                 res.setHeader("Location", "/");
+//                 return res.end();
+//             }
+//             res.end();
+//             break;
+//         default:
+//             break;
+//     }
+// }).listen(3000);
 
 http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    switch (url) {
-        case "/":
-            res.write('<form>"Post Message Form"</form>');
-            res.end();
-            break;
-        case "/message":
-            res.write("<h1>Hello World</h1>");
-            if (method === "POST") {
-                const body = [];
-                req.on("data", (chunk) => {
-                    console.log(chunk);
-                    body.push(chunk);
-                });
-                req.on("end", () => {
-                    const parsedBody = Buffer.concat(body).toString();
-                    fs.writeFileSync("message.txt", parsedBody);
-                });
-                res.statusCode = 302;
-                res.setHeader("Location", "/");
-                return res.end();
-            }
-            res.end();
-            break;
-        default:
-            break;
-    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const q = url.parse(req.url, true).query;
+    const text = q.year + " " + q.month;
+    res.end(text);
 }).listen(3000);
